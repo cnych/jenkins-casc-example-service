d8e3e82c0e4cf784b35476bfca141125759b8383
yangchuanhu <ych_1024@163.com>
2019-06-13 11:20:03 +0800

    init project

---
+++ src/main/java/com/qikqiak/services/example/controller/ExampleController.java
@@ -0,0 +1,25 @@
+package com.qikqiak.services.example.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/example")
+public class ExampleController {
+
+    @Value("${eureka.instance.instanceId}")
+    private String instanceId;
+
+    @GetMapping
+    public String test() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "I'm " + instanceId;
+    }
+
+}
